8aefe75a51401e801ba495a28c2cae25fa7e3b0e
redazures <jwu_nyc@yahoo.com>
2020-09-16 12:34:23 -0400

    fixed

--- src/containers/InstructorsContainer.js
+++ src/containers/InstructorsContainer.js
@@ -3,7 +3,7 @@ import Instructor from '../components/Instructor.js'
 
 function InstructorContainer(props){
 
-    const loves=[{id:1,name:"Vincent"},{id:2,name:"Jules"},{id:3,name:"Jimbo"},{id:4 , name: "Victor"},{id:5, name:"KevX"},{id:6,name:"Ian"},{id:7,name:"Yooj"}]
+    const loves=[{id:1,name:"Vincent"},{id:2,name:"Jules"},{id:3,name:"Jimbo"},{id:4 , name: "Victor"},{id:5, name:"KevX"},{id:6,name:"Ian"},{id:7,name:"YooJ"}]
     let instructors = loves.map(instruct=> <Instructor key={instruct.id} inst={instruct} appClickHandler={props.appClickHandler}/>)
 
     return(
