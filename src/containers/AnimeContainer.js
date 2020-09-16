8a80f95bad0c4006fddf4db1b7d7cc43930b3b1e
redazures <jwu_nyc@yahoo.com>
2020-09-16 08:23:57 -0400

    done

--- src/containers/AnimeContainer.js
+++ src/containers/AnimeContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Instructor from '../components/Instructor.js'
 
 function AnimeContainer(props){
-    // let instructors = lover_names.map(instruct=> <Instructor key={instruct.id} inst={instruct}/>)
+    // let instructors = names.map(instruct=> <Instructor key={instruct.id} inst={instruct}/>)
     console.log("props in anime",props.instructor.name)
     return(
         <>
