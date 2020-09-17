ffaa6cc25d7ff28b01a4e72cb80f74870bda7121
redazures <jwu_nyc@yahoo.com>
2020-09-17 09:11:26 -0400

    fixed most bugs

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,8 @@ class Form extends React.Component{
 
     state={
         name: "",
-        mod:""
+        mod:"",
+        num:8
     }
 
     change=(e)=>{
@@ -16,7 +17,8 @@ class Form extends React.Component{
         return(
             <form onSubmit={(e)=>{//console.log("submit this", e.target.name.value //console.log("this is submit handler",this.props)
                 e.preventDefault()
-                this.props.submit({name: e.target[0].value})
+                this.props.submit({name: e.target.name.value,mod:e.target.mod.value,id:this.state.num})
+                this.setState({num:this.state.num+1})
                 this.setState({name:"",mod:""})
             }}>
                 <input type="text" placeholder="Enter A Lover" name="name" value={this.state.name} onChange={this.change}/><br/><br/>
