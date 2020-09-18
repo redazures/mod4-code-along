94e63db805840273ca2703e488df582422d05e83
redazures <jwu_nyc@yahoo.com>
2020-09-17 22:34:23 -0400

    cannot async, just if statement

--- src/containers/AnimeContainer.js
+++ src/containers/AnimeContainer.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Instructor from '../components/Instructor.js'
 
 function AnimeContainer(props){
-    console.log("props in anime",props.instructor.name)
+    console.log("props in anime",Object.keys(props.instructor))
     return(
+        <>
+        {Object.keys(props.instructor).length===0 ? <h1>No instructors</h1> :
         <>
         <h1>Anime Container</h1>
         <Instructor inst={props.instructor}/>
+        </>
+        }
         </>
     )
 }
